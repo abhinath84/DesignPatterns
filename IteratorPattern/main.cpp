e27fc0dbea0ffaace70bc6baaf8b3fa17a5fae5c
Abhishek Nath <abhi.nath@outlook.com>
2018-04-02 08:19:15 +0530

    Added IteratorPattern project

---
+++ IteratorPattern/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+// extern declaration
+extern void print(Iterator itr);
+
+
+int main(int argc, char *argv[])
+{
+    // Design Patterns by GOF
+    //print(Iterator itr);
+
+    // HeadFirst Design Pattern
+
+    return 0;
+}
